3cd39480e088e3016ff055d712005c67a9379628
Silimon4ikGitHub <115254695+Silimon4ikGitHub@users.noreply.github.com>
2023-01-29 03:07:48 +0100

    Made Catching

--- Assets/Scripts/Bunny.cs
+++ Assets/Scripts/Bunny.cs
@@ -7,6 +7,7 @@ public class Bunny : MonoBehaviour
     [SerializeField] private Vegetables itemScript;
     [SerializeField] private Animator myAnimator;
     [SerializeField] private GameObject handPoint;
+    [SerializeField] private Collider handCollider;
     // Start is called beforeFastIKFabric the first frame update
     void Awake()
     {
@@ -22,7 +23,7 @@ public class Bunny : MonoBehaviour
     private void OnTriggerEnter(Collider collision)
     {
         
-        if (collision.gameObject.GetComponent<Vegetables>())
+        if (handCollider.gameObject.GetComponent<Vegetables>())
         {
             Debug.Log("Here is working");
             handPoint.SetActive(false);
